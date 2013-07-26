bf0a81374db92daf4f105f4ce3080c257b37a342
Bhavya <bhavya@bhavya-Vostro-2520.(none)>
2013-07-26 11:30:58 +0530

    Added queries using active record

--- model/Query.rb
+++ model/Query.rb
@@ -26,5 +26,29 @@ class User < ActiveRecord::Base
    has_many :blogs, :foreign_key => :user_id
   
 end
-
 puts "The Total number of blogs is "+ Blog.count.to_s
+
+print "Username of post 1 is: "
+User.joins(:posts).where("posts.id = '4'").each do |p|
+ puts p.username
+end
+
+#print "User who commented 'Awwesome US pics' is: "
+#User.joins(:posts).where("posts.comment = 'Awesome US pics'").each do |p|
+ #puts p.username
+#end
+
+print "Number of comments on blog 2 is: "
+comm=Post.where(blog_id: '2')
+puts comm.size
+
+
+print "Last 2 comments on blog 2 are: "
+Post.where(blog_id: '2').last(2).each do |m|
+  puts m.comment
+end
+
+print "First 2 comments on blog 2 are: "
+Post.where(blog_id: '2').first(2).each do |m|
+  puts m.comment
+end
