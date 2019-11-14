54a945c21535505ed9453a404a7a514dbd947c11
Bhavya <bhavyac@Bhavyas-MacBook-Pro.local>
2019-11-14 13:43:20 +0530

    update #8

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -2,5 +2,5 @@
 
 # Decrypt the file
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$encrypt_config_password" \
+gpg --quiet --batch --yes --decrypt --passphrase="$secret_key" \
 --output google_auth.development.json google_auth.development.json.gpg
