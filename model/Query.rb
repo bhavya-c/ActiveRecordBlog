5dfd55e491270341acc19ff4ad01c8c3b8fe4140
Bhavya <bhavyac@Bhavyas-MacBook-Pro.local>
2019-11-14 11:55:44 +0530

    Test github actions commit #2

--- model/Query.rb
+++ model/Query.rb
@@ -30,7 +30,7 @@ puts "The Total number of blogs is "+ Blog.count.to_s
 
 print "Username of post 1 is: "
 User.joins(:posts).where("posts.id = '4'").each do |p|
-  puts "test"
+  puts "test #2"
  puts p.username
 end
 
